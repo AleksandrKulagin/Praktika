e9cb03d350b40342575eb257f5ae10f3345f3c67
AleksandrKulagin <60514331+AleksandrKulagin@users.noreply.github.com>
2022-06-30 18:55:46 +0600

    v4
    - Добавлены тесты

---
+++ Calculator/CalculatorTests/UnitTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Calculator.Actions;
+using NUnit.Framework;
+
+namespace calc.Test
+{
+    [TestFixture]
+    public class AdditionTests
+    {
+        [Test]
+        public void CalcTest(double firstNum, double secondNum, double answer)
+        {
+            Add addition = new Add();
+            double Answ = addition.Calculate(firstNum, secondNum);
+            Assert.AreEqual(answer, Answ);
+        }
+    }
+}
